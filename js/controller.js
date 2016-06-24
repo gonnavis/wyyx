00c33967ec556e61b974daf675eeacb2ec86c38c
gonnavis <gonnavis@163.com>
2016-06-24 10:25:40 +0800

    angular base ok

--- js/controller.js
+++ js/controller.js
@@ -11,42 +11,11 @@ angular.module('starter.controllers', [])
 	$s.m={};
 
 	$s.login=function(){
-		im.fn.login({
-			account:$s.m.account,
-			password:$s.m.password,
-			onConnect:function () {
-			    console.log('连接成功');
-			    // console.log(im.nim);
-			    // debugger;
-			    location='#/sessions';
-			},
-			onWillReconnect:function (obj) {
-			    // 此时说明 SDK 已经断开连接, 请开发者在界面上提示用户连接已断开, 而且正在重新建立连接
-			    console.log('即将重连');
-			    console.log(obj.retryCount);
-			    console.log(obj.duration);
-			},
-			onDisconnect:function (error) {
-			    // 此时说明 SDK 处于断开状态, 开发者此时应该根据错误码提示相应的错误信息, 并且跳转到登录页面
-			    console.log('丢失连接');
-			    console.log(error);
-			    if (error) {
-			        switch (error.code) {
-			        // 账号或者密码错误, 请跳转到登录页面并提示错误
-			        case 302:
-			            break;
-			        // 被踢, 请提示错误后跳转到登录页面
-			        case 'kicked':
-			            break;
-			        default:
-			            break;
-			        }
-			    }
-			},
-			onError:function (error) {
-			    console.log(error);
-			},
-		})
+		im.fn.login($s.m.account,$s.m.password);
+	}
+
+	im.ev.onConnect=function(){
+		location='#/sessions';
 	}
 
 })
@@ -55,35 +24,16 @@ angular.module('starter.controllers', [])
 	var $s=$scope;
 	painong.scope.sessions=$s;
 
-	im.fn.sessions($s);
-	$s.data=im.data;
-
-	// console.log(im.nim);
-	// debugger;
-
-	// im.fn.sessions({
-	// 	onSessions:function (sessions) {
-	// 		// debugger;
-	// 	    console.log('收到会话列表', sessions);
-	// 	    for(var i=0;i<sessions.length;i++){
-	// 	    	sessions[i].lastMsg.timeString=new Date(sessions[i].lastMsg.time).toString();
-	// 	    }
-	// 	    $s.sessions = im.nim.mergeSessions($s.sessions, sessions);
-	// 	    updateSessionsUI();
-	// 	},
-	// 	onUpdateSession:function (session) {
-	// 		// debugger;
-	// 	    console.log('会话更新了', session);
-	// 	    $s.sessions = im.nim.mergeSessions($s.sessions, session);
-	// 	    updateSessionsUI();
-	// 	},
-	// })
-
-	// debugger;
-
-	// function updateSessionsUI(){
-	// 	$s.$apply();
-	// }
+	im.ev.onSessions=function(sessions){
+		console.log('sessions');
+		console.log(sessions);
+		$s.data=im.data;
+		$s.$apply();
+	}
+	im.ev.onUpdateSession=function(session){
+		console.log('session');
+		console.log(session);
+	}
 })
 //***********************************************************************************************************************************************************************************************************************************************
 .controller('sessionCtrl', function($scope,pn,im,$location) {
@@ -92,26 +42,22 @@ angular.module('starter.controllers', [])
 	$s.m={};
 
 	$s.account=$location.search().account;
-
-	im.fn.getHistoryMsgs($s.account,function(error,obj){
-	    console.log('获取p2p历史消息' + (!error?'成功':'失败'));
-	    console.log(error);
-	    console.log(obj);
-	    if (!error) {
-	        $s.msgs=obj.msgs.reverse();
-	        $s.$apply();
-	    }
-	})
-
-	$s.send=function(){
-		im.fn.sendText($s.account,$s.m.input,function(error, msg){
-		    console.log(error);
-		    console.log(msg);
-		    console.log('发送' + msg.scene + ' ' + msg.type + '消息' + (!error?'成功':'失败') + ', id=' + msg.idClient);
-	        $s.msgs.push(msg);
-	        $s.$apply();
-		})
+	im.fn.getHistoryMsgs($s.account);
+	im.ev.getHistoryMsgsDone=function(error, obj){
+		$s.msgs=obj.msgs.reverse();
+		$s.$apply();
 	}
 
+	$s.send=function(){
+		im.fn.sendText($s.account,$s.m.input);
+	}
+	im.ev.onUpdateSession=function(session){
 
+	}
+	im.ev.sendMsgDone=function(error, msg){
+		if(!error){
+			$s.msgs.push(msg);
+			$s.$apply();
+		}
+	}
 })
--- js/service.js
+++ js/service.js
@@ -58,75 +58,183 @@ angular.module('starter.services', [])
 
 	var im={};
 	painong.im=im;
+	im.fn={};
+
 	var data = {};
-	var currentScope;
+	var nim;
 
-	var fn={};
+	im.ev={};
+	im.fn.login=function(account,password){
+		nim = NIM.getInstance({
+		    // debug: true,
+		    appKey: appKey,
+		    account: account,
+		    token: password,
+		    onconnect: onConnect,
+		    onwillreconnect: onWillReconnect,
+		    ondisconnect: onDisconnect,
+		    onerror: onError,
+		    onsessions: onSessions,
+		    onupdatesession: onUpdateSession,
+		    onroamingmsgs: onRoamingMsgs,
+		    onofflinemsgs: onOfflineMsgs,
+		    onmsg: onMsg
+		});
+	}
 
-	// login
-		fn.login=function(params){
-			im.nim = NIM.getInstance({
-				appKey:appKey,
-				account:params.account,
-				token:params.password,
-			    onconnect:params.onConnect,
-				onwillreconnect:params.onWillReconnect,
-				ondisconnect:params.onDisconnect,
-				onerror:params.onError
-			});
-			// console.log(nim);
-			// debugger;
-		}
+	im.ev.onConnect=function(){};
+	function onConnect() {
+		console.info('onConnect');
+	    console.log('连接成功');
+	    im.ev.onConnect();
+	}
+    im.ev.onWillReconnect=function(){};
+	function onWillReconnect(obj) {
+		console.info('onWillReconnect');
+	    // 此时说明 SDK 已经断开连接, 请开发者在界面上提示用户连接已断开, 而且正在重新建立连接
+	    console.log('即将重连');
+	    console.log(obj.retryCount);
+	    console.log(obj.duration);
+	    im.ev.onWillReconnect(obj);
+	}
+    im.ev.onDisconnect=function(){};
+	function onDisconnect(error) {
+		console.info('onDisconnect');
+	    // 此时说明 SDK 处于断开状态, 开发者此时应该根据错误码提示相应的错误信息, 并且跳转到登录页面
+	    console.log('丢失连接');
+	    console.log(error);
+	    if (error) {
+	        switch (error.code) {
+	        // 账号或者密码错误, 请跳转到登录页面并提示错误
+	        case 302:
+	            break;
+	        // 被踢, 请提示错误后跳转到登录页面
+	        case 'kicked':
+	            break;
+	        default:
+	            break;
+	        }
+	    }
+	    im.ev.onDisconnect(error);
+	}
+    im.ev.onError=function(){};
+	function onError(error) {
+		console.info('onError');
+	    console.log(error);
+	    im.ev.onError(error);
+	}
 
-	// sessions
-		fn.sessions=function(scope){
-			im.nim.setOptions({
-				onsessions:onSessions,
-				onupdatesession:onUpdateSession,
-			})
-			currentScope=scope;
+    im.ev.onSessions=function(){};
+	function onSessions(sessions) {
+		console.info('onSessions');
+	    console.log('收到会话列表', sessions);
+		for(var i=0;i<sessions.length;i++){
+			sessions[i].lastMsg.timeStr=new Date(sessions[i].lastMsg.time).toString();
 		}
+	    data.sessions = nim.mergeSessions(data.sessions, sessions);
+	    updateSessionsUI();
+	    im.ev.onSessions(sessions);
+	}
+    im.ev.onUpdateSession=function(){};
+	function onUpdateSession(session) {
+		console.info('onUpdateSession');
+	    console.log('会话更新了', session);
+	    data.sessions = nim.mergeSessions(data.sessions, session);
+	    updateSessionsUI();
+	    im.ev.onUpdateSession(session);
+	}
+	function updateSessionsUI() {
+	    // 刷新界面
+	}
 
-		function onSessions(sessions) {
-		    console.log('收到会话列表', sessions);
-		    for(var i=0;i<sessions.length;i++){
-		    	sessions[i].lastMsg.timeString=new Date(sessions[i].lastMsg.time).toString();
-		    }
-		    data.sessions = im.nim.mergeSessions(data.sessions, sessions);
-		    updateSessionsUI();
-		}
-		function onUpdateSession(session) {
-		    console.log('会话更新了', session);
-		    data.sessions = im.nim.mergeSessions(data.sessions, session);
-		    updateSessionsUI();
-		}
-		function updateSessionsUI() {
-		    // 刷新界面
-		    currentScope.$apply();
-		}
 
-	// getHistoryMsgs
-		fn.getHistoryMsgs=function(account,getHistoryMsgsDone){
-			im.nim.getHistoryMsgs({
-			    scene: 'p2p',
-			    to: account,
-			    done: getHistoryMsgsDone
-			});
-		}
+    im.ev.onRoamingMsgs=function(){};
+	function onRoamingMsgs(obj) {
+		console.info('onRoamingMsgs');
+	    console.log('收到漫游消息', obj);
+	    pushMsg(obj.msgs);
+	    im.ev.onRoamingMsgs(obj);
+	}
+    im.ev.onOfflineMsgs=function(){};
+	function onOfflineMsgs(obj) {
+		console.info('onOfflineMsgs');
+	    console.log('收到离线消息', obj);
+	    pushMsg(obj.msgs);
+	    im.ev.onOfflineMsgs(obj);
+	}
+    im.ev.onMsg=function(){};
+	function onMsg(msg) {
+		console.info('onMsg');
+	    console.log('收到消息', msg.scene, msg.type, msg);
+	    pushMsg(msg);
+	    switch (msg.type) {
+	    case 'custom':
+	        onCustomMsg(msg);
+	        break;
+	    case 'notification':
+	        // 处理群通知消息
+	        onTeamNotificationMsg(msg);
+	        break;
+	    default:
+	        break;
+	    }
+	    im.ev.onMsg(msg);
+	}
+	function pushMsg(msgs) {
+	    if (!Array.isArray(msgs)) { msgs = [msgs]; }
+	    var sessionId = msgs[0].sessionId;
+	    data.msgs = data.msgs || {};
+	    data.msgs[sessionId] = nim.mergeMsgs(data.msgs[sessionId], msgs);
+	}
+    im.ev.onCustomMsg=function(){};
+	function onCustomMsg(msg) {
+		console.info('onCustomMsg');
+	    // 处理自定义消息
+	    im.ev.onCustomMsg(msg);
+	}
 
-	// sendText
-		fn.sendText=function(account,text,sendMsgDone){
-			var msg = im.nim.sendText({
-			    scene: 'p2p',
-			    to: account,
-			    text: text,
-			    done: sendMsgDone,
-			});
-			console.log('正在发送p2p text消息, id=' + msg.idClient);
-		}
+	im.fn.sendText=function(account,text){
+		var msg = nim.sendText({
+		    scene: 'p2p',
+		    to: account,
+		    text: text,
+		    done: sendMsgDone
+		});
+		console.log('正在发送p2p text消息, id=' + msg.idClient);
+		pushMsg(msg);
+	}
+    im.ev.sendMsgDone=function(){};
+	function sendMsgDone(error, msg) {
+		console.info('sendMsgDone');
+	    console.log(error);
+	    console.log(msg);
+	    console.log('发送' + msg.scene + ' ' + msg.type + '消息' + (!error?'成功':'失败') + ', id=' + msg.idClient);
+	    pushMsg(msg);
+	    im.ev.sendMsgDone(error, msg);
+	}
+
+	im.fn.getHistoryMsgs=function(account){
+		nim.getHistoryMsgs({
+		    scene: 'p2p',
+		    to: account,
+		    done: getHistoryMsgsDone
+		});
+	}
+    im.ev.getHistoryMsgsDone=function(){};
+	function getHistoryMsgsDone(error, obj) {
+		console.info('getHistoryMsgsDone');
+	    console.log('获取p2p历史消息' + (!error?'成功':'失败'));
+	    console.log(error);
+	    console.log(obj);
+	    if (!error) {
+	        console.log(obj.msgs);
+	    }
+	    im.ev.getHistoryMsgsDone(error, obj);
+	}
 
 	im.data=data;
-	im.fn=fn;
+	im.nim=nim
 
 	return im;
+
 })
